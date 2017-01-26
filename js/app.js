11ad1926e54cff2b8932de3c2b093097446b2db5
kevin-reamer <reamer.kevin@gmail.com>
2017-01-26 13:29:08 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 $(function(){
-	$.getJSON("/form.json", function(form) {
-		var sections = form["form-body"]["sections"];
-		var pageSections = form["form-body"]["pagination-definition"];
-		var indexes = _.pluck(pageSections["section-indexes"]);
-		var valuePairs = [];
+  $.getJSON("formbuilder/form.json", function(form) {
+    var sections = form["form-body"]["sections"];
+    var pageSections = form["form-body"]["pagination-definition"];
+    var indexes = _.pluck(pageSections["section-indexes"]);
+    var valuePairs = [];
 
-		$.each(indexes, function(i, index) {
-			valuePairs.push({index: index, value: null});
-		});
+    $.each(indexes, function(i, index) {
+      valuePairs.push({index: index, value: null});
+    });
 
-		var currentPage = 0;
-		
-		setpage(currentPage);
-	});
+    var currentPage = 0;
+    
+    setpage(currentPage);
+  });
   
   function setpage(pageNumber) {
-  	$("#form").empty();
+    $("#form").empty();
     
     if(pageSections[pageNumber]) {
       var page = _.filter(sections, function(section) { return pageSections[pageNumber].indexOf(section["section-index"]) >= 0 });
@@ -30,43 +30,43 @@ $(function(){
               $("#form").append(htmlTextLabel(field));
               break;
             case 'yes/no':
-            	$("#form").append(htmlYesNo(field));
+              $("#form").append(htmlYesNo(field));
               break;
             case 'date':
-            	$("#form").append(htmlDate(field));
+              $("#form").append(htmlDate(field));
               break;
             case 'input':
               $("#form").append(htmlInput(field));
               break;
           }
         });
-    	});
+      });
     }
   }
   
   //replace these with directives or whatever it's called in angular 2
   function htmlSelect(field) {
-  	var htmlOptions = "";
+    var htmlOptions = "";
     var label = field.label;
     if (label == undefined) { label = field.title; }
     if (field.options[0] == "") { field.options = ["Choice 1","Choice 2","Choice 3"]; }
     
     $.each(field.options, function(index, option) {
-    	htmlOptions += "<option>" + option + "</option>";
+      htmlOptions += "<option>" + option + "</option>";
     });
-  	var htmlReturn = "<p>" + label + "</p>"
-    							 + "<select>" + htmlOptions + "<select>";
-  	return htmlReturn;
+    var htmlReturn = "<p>" + label + "</p>"
+                   + "<select>" + htmlOptions + "<select>";
+    return htmlReturn;
   }
   
   function htmlTextLabel(field) {
-  	var htmlReturn = "<p>" + field.label + "</p>";
+    var htmlReturn = "<p>" + field.label + "</p>";
     return htmlReturn;
   }
   
   function htmlYesNo(field) {
-  	var htmlReturn = "<p>" + field.label + "</p>" +
-    									"<div class='btn-group' role='group' aria-label='" + field.label + "'>" +
+    var htmlReturn = "<p>" + field.label + "</p>" +
+                      "<div class='btn-group' role='group' aria-label='" + field.label + "'>" +
                         "<button type='button' class='btn btn-default' onClick='setValue(field[\"section-index\"], \"yes\")'>Yes</button>" +
                         "<button type='button' class='btn btn-default'>No</button>" +
                       "</div>";
@@ -74,19 +74,19 @@ $(function(){
   }
   
   function htmlDate(field) {
-  	var htmlReturn = "<p>" + field.label + "</p>" +
-    									"<input type='date'>";
+    var htmlReturn = "<p>" + field.label + "</p>" +
+                      "<input type='date'>";
     return htmlReturn;
   }
   
   function htmlInput(field) {
-  	var maxlength = "";
+    var maxlength = "";
     if (field.rules) {
-    	var rule = field.rules[0].split("-");
+      var rule = field.rules[0].split("-");
       if (rule[0] == "limit") { maxlength = rule[1]; }
     }
-  	var htmlReturn = "<p>" + field.label + "</p>" +
-    									"<input type='text' maxlength='" + maxlength + "'>";
+    var htmlReturn = "<p>" + field.label + "</p>" +
+                      "<input type='text' maxlength='" + maxlength + "'>";
     return htmlReturn;
   }
   
